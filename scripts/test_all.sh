a404b69a7db924a29012d17162d450f1a2530aa3
Albert Shi <heng.shi@enflame-tech.com>
2022-03-13 23:52:22 +0800

    merge cargo-test-shell-cmd into test scripts

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -12,4 +12,9 @@ cd ${top_dir_realpath}/build
 # add iree backend execution env
 iree_build_dir="${top_dir_realpath}/iree_build/"
 # export PYTHONPATH=${iree_build_dir}bindings/python:${iree_build_dir}compiler-api/python_package:$PYTHONPATH && cmake --build . --target check-chopper
+
+# regression tests for CRT runtime
+cargo test
+
+# regression tests for entire repo
 cmake --build . --target check-chopper
